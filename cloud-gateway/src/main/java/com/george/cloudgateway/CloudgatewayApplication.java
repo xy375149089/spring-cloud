3fc4ffc8abadc62acf61c6415bbc458fe16c796f
George Xiao (CN) <George.Xiao@homecredit.cn>
2019-02-20 16:05:20 +0800

    add gateway

---
+++ cloud-gateway/src/main/java/com/george/cloudgateway/CloudgatewayApplication.java
@@ -0,0 +1,14 @@
+package com.george.cloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CloudgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudgatewayApplication.class, args);
+	}
+}
